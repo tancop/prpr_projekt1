f9faabb80a84eab799b689c68aedb5f86bf98db0
Teodor Potancok <teodor.potancok@gmail.com>
2024-10-11 15:16:54 +0200

    Add h and switch v to use double pointers

--- main.c
+++ main.c
@@ -6,31 +6,32 @@
 // program po spustení príkazu skončí
 #define RV_END (1)
 
-int v1(FILE *restrict f_data, FILE *restrict f_string, FILE *restrict f_parse)
+int v1(FILE **restrict f_data, FILE **restrict f_string,
+       FILE **restrict f_parse)
 {
-    if (!f_data)
+    if (!*f_data)
     {
-        f_data = fopen("data.txt", "r");
-        if (!f_data)
+        *f_data = fopen("data.txt", "r");
+        if (!*f_data)
         {
             // nepodarilo sa otvoriť súbor
             printf("V1: Neotvorene txt subory.\n");
             return RV_CONTINUE;
         }
     }
-    if (!f_string)
+    if (!*f_string)
     {
-        f_string = fopen("string.txt", "r");
-        if (!f_string)
+        *f_string = fopen("string.txt", "r");
+        if (!*f_string)
         {
             printf("V1: Neotvorene txt subory.\n");
             return RV_CONTINUE;
         }
     }
-    if (!f_parse)
+    if (!*f_parse)
     {
-        f_parse = fopen("parse.txt", "r");
-        if (!f_parse)
+        *f_parse = fopen("parse.txt", "r");
+        if (!*f_parse)
         {
             printf("V1: Neotvorene txt subory.\n");
             return RV_CONTINUE;
@@ -39,7 +40,7 @@ int v1(FILE *restrict f_data, FILE *restrict f_string, FILE *restrict f_parse)
 
     char id_string[6];
 
-    while (fgets(id_string, 7, f_string))
+    while (fgets(id_string, 7, *f_string))
     {
         if (id_string[0] == '\n')
         {
@@ -50,14 +51,14 @@ int v1(FILE *restrict f_data, FILE *restrict f_string, FILE *restrict f_parse)
         {
             printf("ID. mer. modulu: %s\n", id_string);
             // posunieme indikátor na začiatok ďalšieho riadku
-            fseek(f_string, 1, SEEK_CUR);
+            fseek(*f_string, 1, SEEK_CUR);
         }
 
         int h1;
         double h2;
 
         // zahodíme prvé dve čísla cez %*
-        fscanf(f_data, "%*d %*d %d %lf", &h1, &h2);
+        fscanf(*f_data, "%*d %*d %d %lf", &h1, &h2);
         printf("Hodnota 1: %d\nHodnota 2: %lg\n", h1, h2);
 
         printf("Poznámka: ");
@@ -65,7 +66,7 @@ int v1(FILE *restrict f_data, FILE *restrict f_string, FILE *restrict f_parse)
         int c;
 
         // vypisujeme znaky z parse.txt do konca riadku
-        while ((c = fgetc(f_parse)) != '\n')
+        while ((c = fgetc(*f_parse)) != '\n')
         {
             putchar(c);
         }
@@ -73,10 +74,10 @@ int v1(FILE *restrict f_data, FILE *restrict f_string, FILE *restrict f_parse)
         // koniec riadku s poznámkou a prázdny riadok
         printf("\n\n");
     }
-    return RV_END;
+    return RV_CONTINUE;
 }
 
-int v(FILE *restrict f_data, FILE *restrict f_string, FILE *restrict f_parse)
+int v(FILE **restrict f_data, FILE **restrict f_string, FILE **restrict f_parse)
 {
     char subcmd;
 
@@ -92,6 +93,20 @@ int v(FILE *restrict f_data, FILE *restrict f_string, FILE *restrict f_parse)
     }
 }
 
+int h(FILE *f_string)
+{
+    if (!f_string)
+    {
+        // f_string je nulová adresa - neukazuje na súbor
+        printf("H: Neotvoreny subor.\n");
+        return RV_CONTINUE;
+    }
+
+    printf("todo h\n");
+
+    return RV_END;
+}
+
 int main()
 {
     FILE *f_data = NULL, *f_string = NULL, *f_parse = NULL;
@@ -105,7 +120,10 @@ int main()
         switch (cmd)
         {
         case 'v':
-            cmd_return = v(f_data, f_string, f_parse);
+            cmd_return = v(&f_data, &f_string, &f_parse);
+            break;
+        case 'h':
+            cmd_return = h(f_string);
             break;
         default:
             cmd_return = RV_END;
